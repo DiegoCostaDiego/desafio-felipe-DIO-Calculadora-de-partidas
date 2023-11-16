9cd8551812d228597ca5acf7e8dc9c06fa000686
DiegoCostaDiego <diegoeducaunifesp@gmail.com>
2023-11-16 07:44:02 -0300

    formatação

--- teste.js
+++ teste.js
@@ -1,21 +1,20 @@
 //Calculadora de partidass Rankeadas
 // Aqui tentei outra forma de resolver o desafio usando duas funções
 function winCalculation(win, lose) {
-  let winBalance = win - lose
-  return winBalance
+  let winBalance = win - lose;
+  return winBalance;
 }
-
 
 function determinarLevel(winBalance) {
-if (winBalance <= 10)  return "Ferro"
-else if (winBalance <= 20) return "Bronze"
-else if (winBalance <= 50)  return "Prata"
-else if (winBalance <= 80)  return"Ouro"
-else if (winBalance <= 90)  return "Diamente"
-else if (winBalance <= 100)  return "Lendário"
-else return "Imortal"
+  if (winBalance <= 10) return "Ferro";
+  else if (winBalance <= 20) return "Bronze";
+  else if (winBalance <= 50) return "Prata";
+  else if (winBalance <= 80) return "Ouro";
+  else if (winBalance <= 90) return "Diamente";
+  else if (winBalance <= 100) return "Lendário";
+  else return "Imortal";
 }
-let result = winCalculation(105, 33)
-let level = determinarLevel(result)
+let result = winCalculation(105, 33);
+let level = determinarLevel(result);
 
-console.log(`O herói tem o saldo de ${result} está no nível de ${level}`) 
+console.log(`O herói tem o saldo de ${result} está no nível de ${level}`);
